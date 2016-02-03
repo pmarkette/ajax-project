1f12fefc1788a3cd1501853cef1ec0458d6bbe7c
pmarkette <paul@markette.net>
2016-02-02 22:54:24 -0800

    Append image to

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function loadData() {
     // YOUR CODE GOES HERE!
     var address = {street: $("#street").val(), city: $("#city").val()};
     address.url = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address.street  + ', ' + address.city + '&key=AIzaSyAuWTF8z29_K-C5ni7fpSjZixrNO4w1h5k';
-    console.log(address.url);
+    $body.append('<img class="bgimg" src="' + address.url + '">');
 
     return false;
 };
